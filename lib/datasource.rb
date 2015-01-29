56eefd9363ad319c61d6b3e79cc58b8e1b8139fe
Jan Berdajs <mrbrdo@gmail.com>
2015-01-29 01:11:45 +0100

    Logger is thread-safe

--- lib/datasource.rb
+++ lib/datasource.rb
@@ -14,7 +14,7 @@ module Datasource
 
 module_function
   def logger
-    Thread.current[:datasource_logger] ||= Logger.new(STDOUT).tap do |logger|
+    @logger ||= Logger.new(STDOUT).tap do |logger|
       logger.level = Logger::WARN
       logger.formatter = proc do |severity, datetime, progname, msg|
         "[Datasource][#{severity}] - #{msg}\n"
