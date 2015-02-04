1ea2a34c59a86280722e696b9cd7851ce6f6bd2c
Jan Berdajs <mrbrdo@gmail.com>
2015-02-04 06:50:17 +0100

    repository moved

--- lib/generators/datasource/install_generator.rb
+++
@@ -1,8 +0,0 @@
-class Datasource::InstallGenerator < ::Rails::Generators::Base
-  source_root File.expand_path("../templates", __FILE__)
-  desc 'Creates a Datasource Rails initializer'
-
-  def create_serializer_file
-    template 'initializer.rb', 'config/initializers/datasource.rb'
-  end
-end
